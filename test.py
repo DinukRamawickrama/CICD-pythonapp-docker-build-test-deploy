3956c43e5259a7af2ccfd0caad2482c1657f42b0
DinukRamawickrama-github <dinukkaumika199@gmail.com>
2024-09-05 17:31:21 +0530

    tst

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def client():
         yield client
 
 def test_app_is_working(client):
-    print("Running test")
+    print("Running test.")
     response = client.get('/')
     assert response.status_code == 200
     assert b"Hello CICD World!" in response.data
